39900080a4230dfab954ca929c74162a1d9789fb
Lucas Camilo <98404472+LusCamilo@users.noreply.github.com>
2023-02-06 11:27:45 -0300

    update app

--- app/src/main/java/br/senai/sp/jandira/bmicompose/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmicompose/MainActivity.kt
@@ -1,10 +1,13 @@
 package br.senai.sp.jandira.bmicompose
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -27,9 +30,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmicompose.ui.theme.BMIComposeTheme
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.scaleIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import br.senai.sp.jandira.bmicompose.utils.bmiCalculate
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +50,8 @@ class MainActivity : ComponentActivity() {
             BMIComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                   modifier = Modifier.fillMaxSize(),
-                   color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
                     BMICalculator()
                 }
@@ -47,9 +59,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-
 
 
 //
@@ -77,6 +86,7 @@ class MainActivity : ComponentActivity() {
 //
 //}
 
+@OptIn(ExperimentalAnimationApi::class)
 @Preview(
     showSystemUi = true,
 )
@@ -96,11 +106,36 @@ fun BMICalculator() {
 
     }
 
+    var expandSate by remember {
+
+        mutableStateOf(false)
+
+    }
+
+    var bmiScoreState by remember {
+
+        mutableStateOf(0.0)
+
+    }
+
+    var isWeightError by remember {
+
+        mutableStateOf(false)
+
+    }
+    var isHeightError by remember {
+
+        mutableStateOf(false)
+
+    }
+
+    //********************************
+    val weightFocusRequest = FocusRequester()
+
     Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
 
 
-
-        Column (
+        Column(
             Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp),
@@ -127,18 +162,53 @@ fun BMICalculator() {
         Column(
             Modifier
                 .fillMaxWidth()
-                .padding(start = 32.dp, end = 32.dp, top = 10.dp)) {
+                .padding(start = 32.dp, end = 32.dp, top = 10.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+
+        ) {
 
             Column() {
 
-                Text(text = stringResource(id = R.string.weight), modifier = Modifier.padding(bottom = 8.dp))
+                Text(
+                    text = stringResource(id = R.string.weight),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
 
                 OutlinedTextField(
+
                     value = weightState,
-                    onValueChange = {weightState = it},
-                    modifier = Modifier.fillMaxWidth(),
+
+                    onValueChange = {
+
+                        var lastChar =
+                            if (it.length == 0) it
+                            else
+                                it.get(it.length - 1)
+
+                        var newValue =
+                            if (lastChar == '.' || lastChar == ',') it.dropLast(1) else it
+                        weightState = newValue
+
+
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(weightFocusRequest),
+                    leadingIcon = {
+                                  
+                        Icon(imageVector = Icons.Default.DonutLarge, contentDescription = "icone de uma pessoa")
+                                  
+                    },
+                    trailingIcon = {
+
+                        Icon(imageVector = Icons.Default.Warning, contentDescription = "Icone de perigo")
+
+                    },
+                    isError = isWeightError,
+
+                    
                     shape = RoundedCornerShape(16.dp),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     singleLine = true
                 )
 
@@ -148,28 +218,70 @@ fun BMICalculator() {
 
             Column() {
 
-                Text(text = stringResource(id = R.string.height), modifier = Modifier.padding(bottom = 8.dp))
+                Text(
+                    text = stringResource(id = R.string.height),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
 
                 OutlinedTextField(
+
                     value = heightState,
-                    onValueChange = {heightState = it},
+                    onValueChange = {
+
+                        var lastChar =
+                            if (it.length == 0)
+                                it
+                            else
+                                it.get(it.length - 1)
+
+                        var newValue =
+                            if (lastChar == '.' || lastChar == ',')
+                                it.dropLast(1)
+                            else
+                                it
+
+                        heightState = newValue
+                    },
+
                     modifier = Modifier.fillMaxWidth(),
+                    leadingIcon = {
+
+                        Icon(imageVector = Icons.Default.Height, contentDescription = "icone de uma pessoa")
+
+                    },
+                    trailingIcon = {
+
+                        Icon(imageVector = Icons.Default.Warning, contentDescription = "Icone de perigo")
+
+                    },
+                    isError = isHeightError,
                     shape = RoundedCornerShape(16.dp),
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     singleLine = true
-
-
                 )
-
             }
 
             Button(
-                onClick = { /*TODO*/ },
+                onClick = {
+
+                      isWeightError = weightState.length == 0
+
+                      isHeightError = heightState.length == 0
+
+                    if (isHeightError == false && isWeightError == false){
+
+                        bmiScoreState = bmiCalculate(weightState.toInt(), heightState.toDouble())
+                        expandSate = true
+
+                    }
+
+
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 32.dp)
                     .height(48.dp),
-                colors = ButtonDefaults.buttonColors(Color( 40, 150, 30 )),
+                colors = ButtonDefaults.buttonColors(Color(40, 150, 30)),
                 shape = RoundedCornerShape(16.dp)
 
             ) {
@@ -177,66 +289,94 @@ fun BMICalculator() {
                 Text(text = stringResource(id = R.string.button_calculate))
 
             }
-
-
         }
 
         Spacer(modifier = Modifier.height(32.dp))
 
-        Card(
+        AnimatedVisibility(
 
-            Modifier.fillMaxHeight(0.9f),
-            backgroundColor = MaterialTheme.colors.primary,
-            shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+            visible = expandSate,
+            enter = scaleIn() + expandVertically(expandFrom = Alignment.CenterVertically),
+            exit = scaleOut() + shrinkVertically(shrinkTowards = Alignment.CenterVertically)
 
         ) {
-            Column(
 
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxHeight(),
 
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Card(
+
+                Modifier.fillMaxHeight(1f),
+                backgroundColor = MaterialTheme.colors.primary,
+                shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
 
             ) {
+                Column(
 
-                Text(text = stringResource(id = R.string.your_score),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxHeight(),
 
-                    fontSize = 32.sp,
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
 
-                )
-                Text(text = stringResource(id = R.string.BMI),
+                ) {
 
-                    fontSize = 32.sp,
+                    Text(
 
-                )
-                Text(text = stringResource
+                        text = stringResource(id = R.string.your_score),
 
-                    (id = R.string.Description_BMI),
-                    fontSize = 28.sp,
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center
+                        fontSize = 32.sp,
 
-                )
-                Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                    )
 
-                    Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.LightGray)) {
+                    Text(
 
-                        Text(text = stringResource(id = R.string.reset))
+                        text = String.format("%.2f", bmiScoreState),
+                        fontSize = 32.sp
 
-                    }
+                    )
 
-                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = stringResource
 
-                    Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.LightGray)) {
+                            (id = R.string.Description_BMI),
+                        fontSize = 28.sp,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
 
-                        Text(text = stringResource(id = R.string.share))
-                        
+                    )
+                    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+
+                        Button(
+                            onClick = {
+
+                                expandSate = false
+                                weightState = ""
+                                heightState = ""
+                                weightFocusRequest.requestFocus()
+
+                            },
+                            colors = ButtonDefaults.buttonColors(Color.LightGray)
+                        ) {
+
+                            Text(text = stringResource(id = R.string.reset))
+
+                        }
+
+                        Spacer(modifier = Modifier.width(12.dp))
+
+                        Button(
+                            onClick = { /*TODO*/ },
+                            colors = ButtonDefaults.buttonColors(Color.LightGray)
+                        ) {
+
+                            Text(text = stringResource(id = R.string.share))
+
+                        }
                     }
                 }
             }
         }
     }
 }
+
 
---
+++ app/src/main/java/br/senai/sp/jandira/bmicompose/utils/Calculate.kt
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.bmicompose.utils
+
+import kotlin.math.pow
+
+
+fun bmiCalculate(weight: Int, height: Double): Double = weight / ((height / 100 ).pow(2))
+
+
+
